cc14816764fc8d6bd258e1ad4d14404e216c2bad
Leon Dario Herrera Arias <leon.herrera@Quipux.local>
2019-08-08 13:27:36 -0500

    Con swagger
    Conflictos con etiqueta @ApiResponse de la clase PersonaController

--- prqrdrs/src/main/java/edu/java/udem/prqrdrs/controller/PersonaController.java
+++ prqrdrs/src/main/java/edu/java/udem/prqrdrs/controller/PersonaController.java
@@ -2,10 +2,16 @@ package edu.java.udem.prqrdrs.controller;
 
 import edu.java.udem.prqrdrs.dto.PersonaDto;
 import edu.java.udem.prqrdrs.service.PersonaService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/personas")
@@ -22,4 +28,17 @@ public class PersonaController {
     PersonaDto addPersona(@RequestBody PersonaDto personaDto) {
         return personaService.addUser(personaDto);
     }
+    
+    @ApiOperation(
+            value = "Retorna la lista de personas",
+            response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Restorna la lista de personas en JSON"),})
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public List<PersonaDto> findAll() {
+        return personaService.getPersonas();
+    }
+
+    
 }
